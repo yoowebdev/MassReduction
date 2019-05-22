5e20a30aae11b3b3e3da6ebf7d8cbbbc7575a509
yoowebdevlife <yoowebdev@gmail.com>
2019-05-22 18:15:28 -0500

    Uploading files

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# MassReduction
-App for Weight Loss
+Group Project 1. 
+
+Group Members: Amal H, Barbey N, Rick G, Chris Y. 
+
+
+Project title: Weapons of Mass Reduction.
+
+Check it out here: https://barbeyng.github.io/group_project1/
+
+About:
+
+Weapons of Mass Reduction is a web application that helps people live a healthier lifestyle. 
+With the help of the Calorie Count, you can put in the name of the dish you're eating and instantly get the number of calories.
+After you get the result, you can use the Calorie Calculator to know how many calories you burn by doing your daily workout. 
+Don't know where the closest gym to you is located? No worries. You can search for gyms close to you by putting in your zip code or city in the search box of the map that is provided on the app. 
+
+
+
+API's used:
+
+•	With the help of nutritionix API, we are able to pull the number of calories that are in the meal the user search for. 
+
+•	Google Map’s API to search for gyms close to the user’s location. 
+
+
+Design:
+
+•	Bootstrap template.
+
+•	Yazio's "Calories Burned Calculator". 
+
+## Copyright and License
+
+Copyright 2013-2018 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-new-age/blob/gh-pages/LICENSE) license.
---
+++ js/new-age.js
@@ -0,0 +1,124 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
+
+//Calorie Tracker
+$('#submit-btn').click(searchFood);
+function searchFood(event) {
+  event.preventDefault();
+  var userInput = $('#foodInput').val().trim();
+
+  console.log(userInput);
+  var nutritionix = "https://nutritionix-api.p.mashape.com/v1_1/search/" + userInput + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat"
+  $.ajax({
+    url: nutritionix,
+    headers: {
+      "X-Mashape-Key": "3XWHBPKtGMmshHNn3AQ1pj1tDxMCp1YlCKrjsnEh2iA8rmMQJ5",
+      "Accept": "application/json",
+    },
+    method: "GET"
+  }).done(function (response) {
+    console.log(response);
+    console.log(response.hits[0].fields.nf_calories);
+    $("#table > tbody").append("<tr><td>" + userInput + "</td><td>" + response.hits[0].fields.nf_calories + "</td><td>" + response.hits[0].fields.item_name + "</td></tr>");
+  });
+};
+
+//Google Maps API
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 44.983782, lng: -93.180521 },
+    zoom: 13,
+    mapTypeId: 'roadmap'
+  });
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function () {
+    searchBox.setBounds(map.getBounds());
+  });
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function () {
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markers.
+    markers.forEach(function (marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function (place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
